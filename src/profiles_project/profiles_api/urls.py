11030a487f75f01147e4e5f3a58f002c50c89188
Pooja Chugh <pchugh@adobe.com>
2018-12-24 00:06:14 +0530

    Added login API.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -8,6 +8,7 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet )
+router.register('login', views.LoginViewSet, base_name='login')
 """for a model viewset you dont need to specify the base name beacuse rest_framework will auto figure this out"""
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
