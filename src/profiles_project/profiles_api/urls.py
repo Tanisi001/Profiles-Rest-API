7e828707aea31cc6a6b53ae746c372c0fbe17c7c
Pooja Chugh <pchugh@adobe.com>
2018-12-24 00:55:27 +0530

    Added improved permissions to user profile feed items API.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -9,6 +9,7 @@ router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet )
 router.register('login', views.LoginViewSet, base_name='login')
+router.register('feed',views.UserProfileFeedViewSet)
 """for a model viewset you dont need to specify the base name beacuse rest_framework will auto figure this out"""
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
